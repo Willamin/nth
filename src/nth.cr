d4085548a03636a18182cdd89722f5225340357f
Will Lewis <will@wflewis.com>
2017-09-26 14:18:40 -0400

    Use modulo logic instead of string comparisons to determine validity of method call

--- src/nth.cr
+++ src/nth.cr
@@ -9,27 +9,27 @@ struct Int32
   end
 
   def st
-    raise "#{self}.st is invalid" if self.to_s[-1] != '1'
+    raise "#{self}.st is invalid" if self % 10 != 1
     minus_one
   end
 
   def nd
-    raise "#{self}.nd is invalid" if self.to_s[-1] != '2'
+    raise "#{self}.nd is invalid" if self % 10 != 2
     minus_one
   end
 
   def rd
-    raise "#{self}.rd is invalid" if self.to_s[-1] != '3'
+    raise "#{self}.rd is invalid" if self % 10 != 3
     minus_one
   end
 
   def th
-    return minus_one if self.to_s.size > 1 && self.to_s[-2..-1] == "11"
-    return minus_one if self.to_s.size > 1 && self.to_s[-2..-1] == "12"
-    return minus_one if self.to_s.size > 1 && self.to_s[-2..-1] == "13"
-    raise "#{self}.th is invalid" if self.to_s[-1] == '1'
-    raise "#{self}.th is invalid" if self.to_s[-1] == '2'
-    raise "#{self}.th is invalid" if self.to_s[-1] == '3'
+    return minus_one if self > 10 && self % 100 == 11
+    return minus_one if self > 10 && self % 100 == 12
+    return minus_one if self > 10 && self % 100 == 13
+    raise "#{self}.th is invalid" if self % 10 == 1
+    raise "#{self}.th is invalid" if self % 10 == 2
+    raise "#{self}.th is invalid" if self % 10 == 3
     minus_one
   end
 end
